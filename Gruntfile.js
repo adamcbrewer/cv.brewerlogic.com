8a883d79b59ad3129f1899200ab3ae3bfe93d5c4
Adam Brewer <adamcbrewer@gmail.com>
2013-08-04 20:27:27 +0100

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,269 @@
+module.exports = function(grunt) {
+
+    // Load the all the plugins that Grunt requires
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            options: { nospawn: true },
+            env: {
+                // environment files and markup pages
+                files: [
+                    "Gruntfile.js",
+                    "*.php"
+                ],
+                options: { livereload: true },
+                tasks: []
+            },
+            js: {
+                files: [
+                    'assets/js/plugins/*.js',
+                    'assets/js/libs/*.js',
+                    'assets/js/*.js'
+                ],
+                options: { livereload: true },
+                tasks: ['uglify:development']
+            },
+            less: {
+                files: [
+                    'assets/css/less/*.less'
+                ],
+                options: { livereload: true },
+                tasks: ['less:development']
+            }
+        },
+
+        // I need the the css output to originate from the same location.
+        // The difference between the production level is the
+        // level of compression
+        less: {
+            development: {
+                options: {
+                    yuicompress: false,
+                    compress: false
+                },
+                files: [
+                    {
+                        src: ['assets/css/less/core.less'],
+                        dest: 'assets/css/core.css'
+                    },
+                    {
+                        src: ['assets/css/less/core.less'],
+                        dest: 'assets/css/core.css'
+                    }
+                ]
+            },
+            production: {
+                options: {
+                    yuicompress: true,
+                    compress: true
+                },
+                files: [
+                    {
+                        src: ['assets/css/less/core.less'],
+                        dest: 'assets/css/core.css'
+                    },
+                    {
+                        src: ['assets/css/less/core.less'],
+                        dest: 'assets/css/core.css'
+                    }
+                ]
+            }
+        },
+        uglify: {
+
+            // Compile all plugins into a single file.
+            // Run this each time a file is added to the directory.
+            plugins: {
+                options: {
+                    banner: '/*!\n' +
+                        ' * Plugins - <%= grunt.template.today("yyyy-mm-dd H:MM:ss") %>\n' +
+                        ' */\n',
+                    compress: false,
+                    preserveComments: 'all',
+                    beautify: true,
+                    mangle: false,
+                    report: 'min'
+                },
+                files: [
+                    {
+                        src: [
+                            'assets/js/plugins/*.js'
+                        ],
+                        dest: 'assets/js/plugins.js'
+                    }
+                ]
+            },
+
+            // Development-level code
+            development: {
+                options: {
+                    banner: '/*!\n' +
+                        ' * <%= pkg.name %> - <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd H:MM:ss") %>\n' +
+                        ' * Development\n' +
+                        ' * https://github.com/adamcbrewer/launchpad\n *\n' +
+                        ' */\n',
+                    compress: false,
+                    preserveComments: true,
+                    mangle: false,
+                    beautify: true,
+                    report: 'min'
+                },
+                files: [
+                    {
+                        src: [
+                            'assets/js/libs/jquery.js',
+                            'assets/js/plugins.js',
+                            'assets/js/script.js',
+                        ],
+                        dest: 'assets/js/min/script.dev.min.js'
+                    }
+                ]
+            },
+
+            // Production-level code
+            production: {
+                options: {
+                    banner: '/*!\n' +
+                        ' * <%= pkg.name %> - <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd H:MM:ss") %>\n' +
+                        ' * Release\n' +
+                        ' * https://github.com/adamcbrewer/launchpad\n *\n' +
+                        ' */\n',
+                    compress: true,
+                    preserveComments: false,
+                    mangle: false,
+                    report: 'min'
+                },
+                files: [
+                    {
+                        src: [
+                            'assets/js/libs/jquery.js',
+                            'assets/js/plugins.js',
+                            'assets/js/script.js',
+                        ],
+                        dest: 'assets/js/min/script.release.min.js'
+                    }
+                ]
+            }
+        },
+        // TODO
+        // All of this
+        // This should build a custom version of modernizr
+        modernizr: {
+            // [REQUIRED] Path to the build you're using for development.
+            "devFile" : "assets/js/libs/modernizr.js",
+            // [REQUIRED] Path to save out the built file.
+            "outputFile" : "assets/js/libs/modernizr-custom.js",
+            // Based on default settings on http://modernizr.com/download/
+            "extra" : {
+                "shiv" : true,
+                "printshiv" : false,
+                "load" : true,
+                "mq" : false,
+                "cssclasses" : true
+            },
+            // Based on default settings on http://modernizr.com/download/
+            "extensibility" : {
+                "addtest" : false,
+                "prefixed" : false,
+                "teststyles" : false,
+                "testprops" : false,
+                "testallprops" : false,
+                "hasevents" : false,
+                "prefixes" : false,
+                "domprefixes" : false
+            },
+            // By default, source is uglified before saving
+            "uglify" : false,
+            // Define any tests you want to impliticly include.
+            "tests" : [
+                /*
+                "flexbox",
+                "boxshadow",
+                "opacity",
+                "localstorage",
+                "video",
+                "audio",
+                "canvas",
+                "rgba",
+                "flexbox",
+                "draganddrop",
+                "touch",
+                "cssanimations",
+                "csstransitions",
+                "csstransforms3d",
+                "csstransforms",
+                "cssgradients",
+                "backgroundsize"
+                */
+            ],
+            // By default, this task will crawl your project for references to Modernizr tests.
+            // Set to false to disable.
+            "parseFiles" : true,
+            // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
+            // You can override this by defining a "files" array below.
+            "files" : [
+                'assets/js/*.js',
+                'assets/css/less/*.less'
+            ],
+            "excludeFiles": [],
+            // When parseFiles = true, matchCommunityTests = true will attempt to
+            // match user-contributed tests.
+            "matchCommunityTests" : false,
+            // Have custom Modernizr tests? Add paths to their location here.
+            "customTests" : []
+        },
+        copy: {
+            // Copies all the components installed from bower into the libs directory.
+            //
+            installFromBower: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'bower_components/*/*.js',
+                            '!bower_components/*/grunt.js'
+                        ],
+                        dest: 'assets/js/libs/',
+                        filter: 'isFile'
+                    }
+                ]
+            }
+        },
+        clean: {
+            default: [
+                "bower_components/*",
+                "!bower_components/.gitignore",
+                "assets/js/libs/*",
+                "!assets/js/libs/.gitignore",
+                "assets/js/plugins.js",
+                "assets/js/min"
+            ],
+            scripts: [
+                "assets/js/libs/*",
+                "!assets/js/libs/.gitignore",
+                "assets/js/plugins.js",
+                "assets/js/min"
+            ]
+        }
+    });
+
+    grunt.registerTask('default', [
+        'watch'
+    ]);
+
+    // prep files for production
+    grunt.registerTask('build', [
+        'modernizr',
+        'less:production',
+        'uglify:production'
+    ]);
+
+    // Clean up files (testing)
+    grunt.registerTask('reset', [
+        'clean'
+    ]);
+
+};
---
+++ index.php
@@ -0,0 +1,174 @@
+<? include($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"); ?>
+
+	<header>
+		<h1 class="main-title">
+			<span class="highlight">Adam Brewer</span><br />
+			<small>Front-end web developer</small>
+		</h1>
+	</header>
+	<section class="content">
+
+		<section class="section profile">
+
+			<dl>
+				<dt>Portfolio</dt>
+				<dd><a target="_blank" href="http://brewerlogic.com">brewerlogic.com</a></dd>
+
+				<dt>Github</dt>
+				<dd><a target="_blank" href="http://github.com/adamcbrewer">github.com/adamcbrewer</a></dd>
+
+				<dt>LinkedIn</dt>
+				<dd><a target="_blank" href="http://linkedin.com/in/adamcbrewer">linkedin.com/in/adamcbrewer</a></dd>
+
+				<dt>Email</dt>
+				<dd><a href="mailto:adamcbrewer@gmail.com">adamcbrewer@gmail.com</a></dd>
+
+				<dt>Summary</dt>
+				<dd>
+					<p>I'm a fervent Senior front-end Web Developer and Webmaker whose passion lies in all things Javascript, HTML and CSS. I have a fairly comprehensive front-end skill set with some back-end experience as well. I'm always eager to learn anything new.</p>
+					<p>I build semantic, mobile-responsive websites and apps optimised for both speed and performance, utilising good front-end practices and complying with modern web features and standards.</p>
+				</dd>
+			</dl>
+
+		</section>
+
+		<article class="section skills">
+			<header>
+				<h2 class="section-title">Skills</h2>
+			</header>
+			<section>
+				<dt>Languages</dt>
+				<dd>JavaScript, HTML5, CSS3, PHP</dd>
+
+				<dt>Frameworks</dt>
+				<dd>jQuery, MooTools, LESS, SASS, Node.js, CodeIgniter, Laravel, WordPress</dd>
+
+				<dt>Workflow</dt>
+				<dd>Git, Mercurial, Grunt.js, Responsive Design, Command Line</dd>
+			</section>
+		</article>
+
+		<article class="section experience">
+			<header>
+				<h2 class="section-title">Experience</h2>
+			</header>
+			<section>
+				<dt>Aug 2013 – present</dt>
+				<dd>
+					<p>
+						<strong>Freelance Front-end Web Developer</strong>
+					</p>
+				</dd>
+
+				<dt>Dec 2012 – July 2013</dt>
+				<dd>
+					<p>
+						<strong>Saatchi &amp; Saatchi, London</strong><br />
+						A Senior Developer building semantic, mobile-responsive websites
+						and apps optimised for both speed and performance, while also making
+						sure the company utilised good front-end practices, complying with
+						modern web features and standards. I had the pleasure of working with
+						clients such as Cravendale and Toyota, amongst others.
+					</p>
+				</dd>
+
+				<dt>Mar 2011 – Dec 2012</dt>
+				<dd>
+					<p>
+						<strong>Outside Line</strong><br />
+						A mid-level developer building mobile-responsive, semantic and
+						high performance websites and applications across a broad range
+						of clients. I have also helped build in-house applications for
+						the company, including a database accessibility app for account
+						managers – as well as supervising incoming juniors. Client experience
+						includes: Becks, Budweiser, Nominet, Castello, Lurpak, Aunt
+						Bessies and Virgin Galactic.
+					</p>
+				</dd>
+
+				<dt>Nov 2010 – Feb 2011</dt>
+				<dd>
+					<p>
+						<strong>Brewer's Data Applications</strong><br />
+						Moved my father's business online by building both the CMS and
+						front-end for his web app after only a few short months in the
+						industry. My work has improved much since then, but the challenge
+						and learning curve at the time, to me, was incredible – it opened
+						doors.
+					</p>
+				</dd>
+
+				<dt>Aug 2010 – Nov 2010</dt>
+				<dd>
+					<p>
+						<strong>QuickOnline</strong><br />
+						A very insightful internship, creating websites and emailers for clients
+						such as Knead, The Foodie, CTBI, Acquire, ProjectX, CTFW, 2Xist, Nudo,
+						I Love Coffee and Spier Wines.
+					</p>
+				</dd>
+
+				<dt>Jun 2010 – Jul 2010</dt>
+				<dd>
+					<p>
+						<strong>IMS Nielson</strong><br />
+						A CMS for distributing desktop software updates via FTP with built-in email
+						notification to clients. Users are required to verify their account status in
+						order to proceed with the update.
+					</p>
+				</dd>
+
+				<dt>Feb 2010 – May 2010</dt>
+				<dd>
+					<p>
+						<strong>20fourlabs.com</strong><br />
+						Internship. The start of it all.
+					</p>
+				</dd>
+
+				<dt>Feb 2005 – Nov 2009</dt>
+				<dd>
+					<p>
+						<strong>Aircraft Technician in the British Army</strong><br />
+						I attained the position of Corporal, supervising teams in maintaining
+						aircraft in the UK, Germany and Iraq.
+					</p>
+				</dd>
+			</section>
+		</article>
+
+		<article class="section experience">
+			<header>
+				<h2 class="section-title">Education</h2>
+			</header>
+			<section>
+				<dt>Sep 2009 – Nov 2009</dt>
+				<dd>
+					<p>
+						<strong>LPIC-1 - Linux Administrator L1</strong><br />
+						Completed during my last few months in the British Army.
+					</p>
+				</dd>
+
+				<dt>Apr 2005 – Oct 2006</dt>
+				<dd>
+					<p>
+						<strong>REME School of Aeronautical Engineering</strong><br />
+						Advanced apprenticeship in maintaining rotary aircraft in the British Army NVQ level 3 in Engineering Maintenance.
+					</p>
+				</dd>
+
+				<dt>Jan 2003 – Dec 2003</dt>
+				<dd>
+					<p>
+						<strong>University of Cape Town</strong><br />
+						1<sup>st</sup> Year Mechanical Engineering.
+					</p>
+				</dd>
+			</section>
+		</article>
+
+
+	</section>
+
+<? include($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"); ?>
